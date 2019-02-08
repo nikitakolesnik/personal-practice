d66aac8926b1762c9b80271204c58ec8c0baa593
Nikita Kolesnik <45475434+nikitakolesnik@users.noreply.github.com>
2019-02-08 06:14:03 -0800

    Update 1DArrayRotation.cpp

--- 1DArrayRotation.cpp
+++ 1DArrayRotation.cpp
@@ -1,7 +1,10 @@
+// Rotates a one-dimensional array using three different algorithms of varying time complexity
+
 #include <iostream>
 
 
-const int size = 10;
+const int size   = 10; // Size of the consecutive-ascending int array to generate
+const int rotate = 1;  // How many positions to rotate it by, using each algorithm
 
 
 void printArray(int arr[])
@@ -11,34 +14,31 @@ void printArray(int arr[])
 	std::cout << "\n";
 }
 
-//void rotateBruteForce(int * arr)
-//void rotateBruteForce(int arr[5])
+// Exponential time complexity iterative solution
 void rotateBruteForce(int arr[], int rotate)
 {
 	int prev, temp;
+	
 	for (int i = 0; i < rotate; i++)
 	{
 		prev = arr[size - 1];
 		for (int j = 0; j < size; j++)
 		{
-			temp = arr[j];
+			temp   = arr[j];
 			arr[j] = prev;
-			prev = temp;
-
-			//printArray(arr);
+			prev   = temp;
 		}
 	}
 }
 
+// 2n time & space complexity iterative solution
 void rotateCopy(int arr[], int rotate)
 {
-	int copy[size];
-	int index;
+	int copy[size], index;
 
 	for (int i = 0; i < size; i++)
 	{
 		index = (i + rotate) % size;
-		//std::cout << "Writing " << arr[i] << " to index " << index << "\r\n";
 		copy[index] = arr[i];
 	}
 
@@ -46,27 +46,28 @@ void rotateCopy(int arr[], int rotate)
 		arr[i] = copy[i];
 }
 
+// Linear time complexity recursive solution
 void rotateReverseHelper(int arr[], int start, int end)
 {
 	int temp;
+	
 	while (start++ < end--)
 	{
-		temp = arr[start];
+		temp       = arr[start];
 		arr[start] = arr[end];
-		arr[end] = temp;
+		arr[end]   = temp;
 	}
 }
 
 void rotateReverse(int arr[], int rotate)
 {
-	rotateReverseHelper(arr, 0, rotate - 1);
-	rotateReverseHelper(arr, rotate, size - 1);
-	rotateReverseHelper(arr, 0, size - 1);
+	rotateReverseHelper(arr,      0, rotate - 1);
+	rotateReverseHelper(arr, rotate,   size - 1);
+	rotateReverseHelper(arr,      0,   size - 1);
 }
 
 int main()
 {
-	int rotate = 1;
 	int arr[size];
 
 	for (int i = 0; i < size; i++)
@@ -74,13 +75,16 @@ int main()
 
 	// Rotate the array by the same amount each time, in different ways:
 
-	printArray(arr);
+	printArray(arr); // Original
+	
 	rotateBruteForce(arr, rotate);
-	printArray(arr);
+	printArray(arr); // Rotated r * 1
+	
 	rotateCopy(arr, rotate);
-	printArray(arr);
+	printArray(arr); // Rotated r * 2
+	
 	rotateReverse(arr, rotate);
-	printArray(arr);
+	printArray(arr); // Rotated r * 3
 
 	std::cin.get();
 }
