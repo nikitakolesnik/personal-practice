ca8d62f2a7c88dacbddf4122df3601304dc410b6
Nikita Kolesnik <45475434+nikitakolesnik@users.noreply.github.com>
2019-08-24 16:21:23 -0700

    added comments

--- StringReverseWithoutStringLibrary.cpp
+++ StringReverseWithoutStringLibrary.cpp
@@ -4,14 +4,16 @@
 
 void mystrrev(char* s)
 {
+	// Find out how long it is
 	int len = 0;
 	char* trav = s;
-
 	while (*trav++)
 		len++;
 
+	// Only need to iterate for half the length, odd-length-case is automatically handled (ex: length=9, truncated division result is 4, 5th spot correctly ignored)
 	for (int i = 0; i < len/2; i++)
 	{
+		// Swap str[i] & str[1+len-i] without using extra variable
 		*(s+i) ^= *(s + len - i - 1);
 		*(s + len - i - 1) ^= *(s + i);
 		*(s + i) ^= *(s + len - i - 1);
