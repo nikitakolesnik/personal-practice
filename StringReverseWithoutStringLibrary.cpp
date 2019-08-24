3b9daaadeec84ba4a02ddc34faaca865b7031dc8
Nikita Kolesnik <45475434+nikitakolesnik@users.noreply.github.com>
2019-08-24 16:59:47 -0700

    Update StringReverseWithoutStringLibrary.cpp

--- StringReverseWithoutStringLibrary.cpp
+++ StringReverseWithoutStringLibrary.cpp
@@ -1,29 +1,29 @@
-// A completely worthless, not applicable exercise
+//A completely worthless, not applicable exercise
 
 #include <iostream>
 
-void mystrrev(char* s)
+void mystrrev(char* str)
 {
 	// Find out how long it is
-	char* trav = s;
-	while (*trav)
-		++trav;
-	int len = trav - s;
+	char* walk = str;
+	while (*walk)
+		++walk;
+	int len = walk - str;
 
 	// Only need to iterate for half the length, odd-length-case is automatically handled (ex: length=9, truncated division result is 4, 5th spot correctly ignored)
 	for (int i = 0; i < len / 2; i++)
 	{
 		// Swap str[i] & str[1+len-i] without using extra variable
-		*(s + i) ^= *(s + len - i - 1);
-		*(s + len - i - 1) ^= *(s + i);
-		*(s + i) ^= *(s + len - i - 1);
+		*(str+i) ^= *(str+len-i-1);
+		*(str+len-i-1) ^= *(str+i);
+		*(str+i) ^= *(str+len-i-1);
 	}
 }
 
 int main()
 {
-	char test[29];
-	strcpy_s(test, "This is a great use of time.");
-	mystrrev(test);
-	std::cout << test << '\n';
+	char str[29];
+	strcpy_s(str, "This is a great use of time.");
+	mystrrev(str);
+	std::cout << str << '\n';
 }
