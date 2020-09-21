ee2ea98b6860817d89a42df11c7e8cd4ab03a861
Nikita Kolesnik <45475434+nikitakolesnik@users.noreply.github.com>
2020-09-21 14:58:25 -0700

    Update StringReverseWithoutStringLibrary.cpp

--- StringReverseWithoutStringLibrary.cpp
+++ StringReverseWithoutStringLibrary.cpp
@@ -7,7 +7,7 @@ void my_str_rev(char* str)
 		++walk;
 	int len = walk - str;
 
-	for (int i = 0; i < len / 2; i++) // Only need to iterate for half the length, odd-length-case is automatically handled (ex: length=9, truncated division result is 4, 5th spot correctly ignored)
+	for (int i = 0; i < len / 2; i++) // Only need to iterate for half the length; odd-length-case is automatically handled (ex: length=9, truncated division result is 4, 5th spot correctly ignored)
 	{
 		*(str + i) ^= *(str + len - i - 1); // XOR swap
 		*(str + len - i - 1) ^= *(str + i);
