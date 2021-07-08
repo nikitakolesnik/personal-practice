c53e363984958e819bca26f8e73dfeeed01747bc
Nikita Kolesnik <45475434+nikitakolesnik@users.noreply.github.com>
2021-07-08 16:07:47 -0700

    fixed case of length<2 and cleaned up solution

--- SmallestIntegerNotOccurringInList.cs
+++ SmallestIntegerNotOccurringInList.cs
@@ -9,24 +9,33 @@ class Solution
 {
     public int solution(int[] A)
     {
-        Array.Sort(A); // Allows this to be solved without visiting every member or using extra space
-        int last = A[A.Length - 1];
+        // Since we index into [length-1] and [length-2] later, make sure they're valid
+        
+        if (A.Length == 0)
+            return 1;
+        if (A.Length == 1)
+            return (A[0] != 1) ? 1 : 2;
+        
+        // Ok, can continue. Sorting allows this to be solved without visiting every member or using extra space
+        
+        Array.Sort(A);
 
-        if (A[0] > 1 || last < 1) // Check the two cases in which we can immediately just return 1. Examples being {2,3,4} and {-4,-3,-2} respectively
+        // Check the two cases in which we can immediately just return 1. Examples being {2,3,4} and {-4,-3,-2} respectively
+        
+        if (A[0] > 1 || A[A.Length - 1] < 1)
             return 1;
 
         // If the list is consecutive, return highest + 1. We can know this without traversing the list at all.
+        
         if (A[A.Length - 1] - A[0] == A.Length - 1)
             return A[A.Length - 1] + 1;
 
-        // If the list is non-consecutive, and the result is within bounds of the lowest and highest members
-        int min = int.MaxValue;
-        for (int i = A.Length - 2; i >= 0; i--) // Going backwards, starting with the second-to-last number
-        {
-            if (last - A[i] > 1) // If these two elements have a gap larger than 1,
-                min = A[i] + 1;  // Then the new minimum is the lowest number in this gap
-            last = A[i];
-        }
-        return min; // this has to have been updated, since the consecutive case was handled above
+        // If the list is non-consecutive, and the result is within bounds of the lowest and highest members, find the lowest numbers with a gap of >1
+        
+        for (int i = 0; i < A.Length-2; i++)
+            if (A[i+1] - A[i] > 1)
+                return A[i] + 1;
+        
+        return -1; // unreachable
     }
 }
